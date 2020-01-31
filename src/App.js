279f9665617b53477033ac36c954c15210a04af3
viveloper <usik1234@gmail.com>
2020-01-31 16:56:54 +0900

    Scream Card Details

--- src/App.js
+++ src/App.js
@@ -1,33 +1,56 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+// pages
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
+// components
 import Navbar from './components/Navbar';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={home} />
-            <Route exact path="/login" component={login} />
-            <Route exact path="/signup" component={signup} />
-            <Route
-              path=""
-              component={() => (
-                <div>
-                  <h1>Page Not Found</h1>
-                </div>
-              )}
-            />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+              <Route
+                path=""
+                component={() => (
+                  <div>
+                    <h1>Page Not Found</h1>
+                  </div>
+                )}
+              />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
 
 export default class home extends Component {
+  state = {
+    screams: null
+  };
+  componentDidMount() {
+    axios
+      .get('/screams')
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          screams: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
   render() {
     return (
-      <div>
-        <h1>home page</h1>
-      </div>
+      <Grid container spacing={16}>
+        <Grid item sm={8} xs={12}>
+          <p>Content...</p>
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile...</p>
+        </Grid>
+      </Grid>
     );
   }
 }
