9a0f855c5014121a0485f5b7c1ed5e79e8f6ceb3
viveloper <usik1234@gmail.com>
2020-01-31 15:45:13 +0900

    Getting Started With React

--- src/App.js
+++ src/App.js
@@ -4,24 +4,28 @@ import './App.css';
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route exact path="/" component={home} />
-          <Route exact path="/login" component={login} />
-          <Route exact path="/signup" component={signup} />
-          <Route
-            path=""
-            component={() => (
-              <div>
-                <h1>Page Not Found</h1>
-              </div>
-            )}
-          />
-        </Switch>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route exact path="/login" component={login} />
+            <Route exact path="/signup" component={signup} />
+            <Route
+              path=""
+              component={() => (
+                <div>
+                  <h1>Page Not Found</h1>
+                </div>
+              )}
+            />
+          </Switch>
+        </div>
       </Router>
     </div>
   );
